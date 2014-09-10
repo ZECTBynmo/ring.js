a8190ddb1a8a6ec4c0726891175bacbb741bd306
Mike Vegeto <michael.vegeto@gmail.com>
2014-09-10 01:27:44 -0400

    fix json

---
+++ component.js
@@ -0,0 +1,15 @@
+{
+  "name": "ring.js",
+  "repo": "ZECTBynmo/ring.js",
+  "version": "2.0.1",
+  "description": "ring.js component",
+  "keywords": [
+    "ring.js",
+    "component"
+  ],
+  "main": "ring.js",
+  "scripts": [
+    "ring.js"
+  ],
+  "license": "MIT"
+}
