54ea17122410c55b8a50408fb9f79fb7f8267254
Mike Vegeto <michael.vegeto@gmail.com>
2014-09-10 01:24:04 -0400

    componentjs config file

---
+++ component.js
@@ -0,0 +1,14 @@
+{
+  "name": "ring.js",
+  "repo": "ZECTBynmo/ring.js",
+  "version": "2.0.1",
+  "description": "ring.js component",
+  "keywords": [
+    "ring.js",
+    "component"
+  ],
+  "main": "ring.js",
+  "scripts": [
+    "ring.js"
+  ],
+  "license": "MIT"
